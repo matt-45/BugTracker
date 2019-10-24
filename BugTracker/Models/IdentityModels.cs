74809de73558800eb4c1465b6c30bfbd15ca30e0
matt-45 <53614616+matt-45@users.noreply.github.com>
2019-10-24 09:16:53 -0400

    Initial commit

---
+++ BugTracker/Models/IdentityModels.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Data.Entity;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+
+namespace BugTracker.Models
+{
+    // You can add profile data for the user by adding more properties to your ApplicationUser class, please visit https://go.microsoft.com/fwlink/?LinkID=317594 to learn more.
+    public class ApplicationUser : IdentityUser
+    {
+        [Display(Name="First Name")]
+        [StringLength(50, MinimumLength = 1, ErrorMessage ="First Name must be between 1 and 50 characters")]
+        public string FirstName { get; set; }
+        [Display(Name="Last Name")]
+        [StringLength(50, MinimumLength = 1, ErrorMessage = "Last Name Name must be between 1 and 50 characters")]
+        public string LastName { get; set; }
+        [Display(Name="Display Name")]
+        [StringLength(50, MinimumLength = 1, ErrorMessage = "Display Name must be between 1 and 50 characters")]
+
+        public string DisplayName { get; set; }
+        public virtual ICollection<TicketComment> TicketComments { get; set; }
+        public virtual ICollection<Project> Projects { get; set; }
+        public virtual ICollection<TicketAttachment> TicketAttachments { get; set; }
+        public virtual ICollection<TicketHistory> TicketHistorys { get; set; }
+        public virtual ICollection<TicketNotification> TicketNotifications { get; set; }
+
+        public ApplicationUser()
+        {
+            TicketComments = new HashSet<TicketComment>();
+            Projects = new HashSet<Project>();
+            TicketAttachments = new HashSet<TicketAttachment>();
+            TicketHistorys = new HashSet<TicketHistory>();
+            TicketNotifications = new HashSet<TicketNotification>();
+        }
+
+        public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<ApplicationUser> manager)
+        {
+            // Note the authenticationType must match the one defined in CookieAuthenticationOptions.AuthenticationType
+            var userIdentity = await manager.CreateIdentityAsync(this, DefaultAuthenticationTypes.ApplicationCookie);
+            // Add custom user claims here
+            return userIdentity;
+        }
+    }
+
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext()
+            : base("DefaultConnection", throwIfV1Schema: false)
+        {
+        }
+
+        public static ApplicationDbContext Create()
+        {
+            return new ApplicationDbContext();
+        }
+    }
+}
---
+++ BugTracker/Models/Project.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BugTracker.Models
+{
+    public class Project
+    {
+        public Project() 
+        {
+            Tickets = new HashSet<Ticket>();
+            Users = new HashSet<ApplicationUser>();
+        }
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+
+        public virtual ICollection<Ticket> Tickets { get; set; }
+        public ICollection<ApplicationUser> Users { get; set; }
+
+
+    }
+}
